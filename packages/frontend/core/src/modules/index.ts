bed11370a1ce7d95f74fb53bf6f1713cd9f2de6c
Jimmfly <yangjinfei001@gmail.com>
2025-01-13 17:41:22 +0800

    refactor(core): add share menu service

--- packages/frontend/core/src/modules/index.ts
+++ packages/frontend/core/src/modules/index.ts
@@ -37,6 +37,7 @@ import { configurePeekViewModule } from './peek-view';
 import { configurePermissionsModule } from './permissions';
 import { configureQuickSearchModule } from './quicksearch';
 import { configureShareDocsModule } from './share-doc';
+import { configureShareMenuModule } from './share-menu';
 import { configureShareSettingModule } from './share-setting';
 import {
   configureCommonGlobalStorageImpls,
@@ -100,4 +101,5 @@ export function configureCommonModules(framework: Framework) {
   configureAIButtonModule(framework);
   configureTemplateDocModule(framework);
   configureBlobManagementModule(framework);
+  configureShareMenuModule(framework);
 }
